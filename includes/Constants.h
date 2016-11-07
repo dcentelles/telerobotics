979ab6df96018626a2b71fe8a98e94437d42f6e5
Diego Centelles <dieguscentbelt@gmail.com>
2016-11-07 13:48:36 +0100

    added constants header

---
+++ includes/Constants.h
@@ -0,0 +1,21 @@
+/*
+ * Constants.h
+ *
+ *  Created on: 7 nov. 2016
+ *      Author: diego
+ */
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+
+namespace dcauv {
+
+#define IMG_TRUNK_INFO_SIZE 2
+#define IMG_FIRST_TRUNK_FLAG 0x8000
+#define MAX_IMG_SIZE 32767 //(2^15-1)
+#define IMG_CHKSUM_SIZE 4
+
+}
+
+#endif /* CONSTANTS_H_ */
